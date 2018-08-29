1e8c3b59704ab799de14d83c62ce2d5ae6bf03b4
Glen Holcomb <MentalLabor@gmail.com>
2018-08-29 12:43:06 -0600

    Sort of First

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule ExPaypal.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_paypal,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {ExPaypal, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test),      do: ["lib", "test/support"]
+  defp elixirc_paths(_),          do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, ">= 0.0.0"},
+      {:poison, ">= 0.0.0"}
+    ]
+  end
+end
