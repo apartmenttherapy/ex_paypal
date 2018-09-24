ccae154fd57c720743a4571ec700a628b832b3df
Glen Holcomb <MentalLabor@gmail.com>
2018-09-24 11:19:01 -0600

    Adds `partner_id` config for dev

--- config/dev.exs
+++ config/dev.exs
@@ -5,4 +5,6 @@ config :ex_paypal,
   client_id: "AfeFhW9EbH1pT31yY3B4fMpd_R-wmueY8zf2RAAbqIXMbQr--gmlXzY5I1whHdOL_Kkt69bWa44SQKn9",
   client_secret: System.get_env("PAYPAL_SECRET"),
   bn_code: System.get_env("PAYPAL_BN_CODE"),
-  client_meta: System.get_env("PAYPAL_CLIENT_METADATA")
+  client_meta: System.get_env("PAYPAL_CLIENT_METADATA"),
+  partner_id: System.get_env("PAYPAL_PARTNER_ID")
+
